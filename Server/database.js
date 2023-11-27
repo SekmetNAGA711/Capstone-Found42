4c9f5fded98ed8ab2286f6ba933e2216e3764399
AbbyANG <bena.ao310@gmail.com>
2023-11-27 11:33:44 -0500

    Update database configuration for Heroku

--- Server/database.js
+++ Server/database.js
@@ -1,7 +1,27 @@
-require('dotenv').config()
-const CONNECTION_STRING = process.env.CONNECTION_STRING
-const {Sequelize} = require('sequelize')
+// require('dotenv').config()
+// const CONNECTION_STRING = process.env.CONNECTION_STRING
+// const {Sequelize} = require('sequelize')
 
-const db = new Sequelize(CONNECTION_STRING)
+// const db = new Sequelize(CONNECTION_STRING)
 
-module.exports = db
+// module.exports = db
+
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+// Check for the Heroku environment variable, otherwise use the development connection string
+const CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://postgres:admin@localhost:5432/Health-is-Wealth';
+
+const db = new Sequelize(CONNECTION_STRING, {
+  dialect: 'postgres', // This explicitly sets the dialect for Sequelize
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Necessary for Heroku to allow connection
+    }
+  },
+  logging: false // Optional: Disable SQL query logging
+});
+
+module.exports = db;
+
